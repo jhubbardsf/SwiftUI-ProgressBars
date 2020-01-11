9db404cd5a7f1481cc34b61c0d87f3cfeabf9380
Joshua Hubbard <jhubardsf@gmail.com>
2020-01-10 21:47:35 -0500

    Initial Commit

---
+++ ProgressBars/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProgressBars
+//
+//  Created by Josh Hubbard on 1/10/20.
+//  Copyright © 2020 YeetBox. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProgressBarsTests/ProgressBarsTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProgressBarsTests.swift
+//  ProgressBarsTests
+//
+//  Created by Josh Hubbard on 1/10/20.
+//  Copyright © 2020 YeetBox. All rights reserved.
+//
+
+import XCTest
+@testable import ProgressBars
+
+class ProgressBarsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
